c328d563be19ab978fddba201760a77456c30ea9
ErvinRivas <ervin.rivas@correounivalle.edu.co>
2022-10-06 20:58:39 -0500

    modified branch, introduction

---
+++ app.js
@@ -0,0 +1,9 @@
+//let numero
+//numero = 10
+//alert(numero)
+//document.write(<h2>Bienvenidos</h2>)
+let nombre = prompt("Ingrese el su nombre :")
+let num1 = parseInt(prompt("Ingrese el numero 1:"))
+let num2 = parseInt(prompt("Ingrese el numero 2:"))
+let suma = num1 + num2
+alert(nombre+" La suma de los numeros ingresados es: "+ suma)
